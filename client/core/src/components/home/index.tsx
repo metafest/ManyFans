8eefbca0e37a094e30e6f0f36312678fda768193
BalajiSriraman <balajisrdev@gmail.com>
2025-02-09 00:17:56 +0530

    Update home component text for clarity and impact

--- client/core/src/components/home/index.tsx
+++ client/core/src/components/home/index.tsx
@@ -10,11 +10,12 @@ export function Home() {
     <div className="flex flex-col items-center dark">
       <BackgroundLines svgOptions={{ duration: 7 }} className="flex items-center justify-center w-full flex-col px-4">
         <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-          ManyFans, <br /> is better than OnlyFans.
+          ManyFans!
+          <br />
         </h2>
         <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-400 text-center">
-          Experience the thrill of exclusive content. who know exactly what you
-          desire.
+          Many is better than one. Experience the thrill of exclusive content.
+          {/* who know exactly what you desire. */}
         </p>
 
         <CoolMode
