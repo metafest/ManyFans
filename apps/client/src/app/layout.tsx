dba8c9a971eda2eeaf76760d457f89125b0b994c
Chandan SM <chandan.sm.dev@gmail.com>
2025-02-14 11:01:08 +0530

    Implemented Light & Dark Mode

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Theme } from "./theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,13 +33,9 @@ export default function RootLayout({
       >
         <head />
         <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
+          <ThemeProvider>
             {children}
+            <Theme />
           </ThemeProvider>
         </body>
       </html>
---
+++ apps/client/src/app/theme.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { Sun, Moon } from "lucide-react";
+
+export function Theme() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
+  if (!mounted) return <div className="min-h-screen bg-white dark:bg-black" />;
+  return (
+    <button
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="mt-5 p-2 rounded-full bg-gray-200 dark:bg-gray-800 transition  absolute top-1 right-5"
+        >
+        {theme === "dark" ? <Sun color="black" size={24} /> : <Moon size={24} />}
+    </button>
+  );
+}
