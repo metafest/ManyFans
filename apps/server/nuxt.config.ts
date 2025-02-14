84425b4d5526035b215bf0d921dd2a06e96d9bd5
BalajiSriraman <balajisrdev@gmail.com>
2025-02-14 11:33:30 +0530

    initialize Nuxt app with basic configuration and setup files

--- apps/server/nuxt.config.ts
+++ apps/server/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+<<<<<<< HEAD
   compatibilityDate: "2024-07-30",
   future: { compatibilityVersion: 4 },
   modules: ["@nuxthub/core", "@nuxt/ui"],
@@ -16,3 +17,8 @@ export default defineNuxtConfig({
   },
   devtools: { enabled: true },
 });
+=======
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
+>>>>>>> 08dbcc6 (initialize Nuxt app with basic configuration and setup files)
