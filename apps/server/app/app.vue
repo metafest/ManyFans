2aecef3df05590d3828c0ca99ec1a95b85362a66
BalajiSriraman <balajisrdev@gmail.com>
2025-02-15 00:04:52 +0530

    remove unused components, API endpoints, and update nuxt.config to streamline the application

--- apps/server/app/app.vue
+++ apps/server/app/app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 <<<<<<< HEAD
+<<<<<<< HEAD
 useHead({
   link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
@@ -11,28 +12,36 @@ const title = "ManyFans Server";
 =======
 const { getImages } = useFile()
 
+=======
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
 useHead({
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
+<<<<<<< HEAD
 const title = 'NuxtHub Image Gallery Starter'
 const description = 'A Nuxt template to build your full-stack image gallery application on the edge.'
 >>>>>>> cf91573 (add server configuration, API endpoints, and image handling components)
+=======
+const title = "ManyFans Server";
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
 
 useSeoMeta({
   title,
   ogTitle: title,
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
   // ogDescription: description,
   // ogSiteName: "NuxtHub image gallery template",
   // ogImage: "https://image-gallery.nuxt.dev/social-card.png",
   // twitterCard: "summary_large_image",
 });
+<<<<<<< HEAD
 </script>
 
 <template>
@@ -48,16 +57,18 @@ useSeoMeta({
 })
 
 await getImages()
+=======
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
 </script>
 
 <template>
-  <div
-    class="bg-black min-h-[100dvh] overflow-x-auto relative"
-    :class="{ 'flex flex-col md:block': $router.currentRoute.value.fullPath !== '/' }"
-  >
+  <div class="bg-black min-h-[100dvh] overflow-x-auto relative">
     <UNotifications />
     <NuxtPage />
+<<<<<<< HEAD
     <ImageThumbnailList :class="$router.currentRoute.value.fullPath !== '/' ? 'opacity-100 z-[9999]' : 'opacity-0 z-[-1]'" />
 >>>>>>> cf91573 (add server configuration, API endpoints, and image handling components)
+=======
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
   </div>
 </template>
--- apps/server/app/pages/index.vue
+++ apps/server/app/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
   <div class="p-5">
     <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
       <UInput v-model="q" placeholder="Filter files..." />
@@ -33,6 +36,7 @@
       class="mt-4"
     />
   </div>
+<<<<<<< HEAD
 </template>
 
 <script setup lang="ts">
@@ -105,3 +109,71 @@ const filteredRows = computed(() => {
   </main>
 </template>
 >>>>>>> cf91573 (add server configuration, API endpoints, and image handling components)
+=======
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/images/");
+
+const columns = [
+  {
+    key: "pathname",
+    label: "Name",
+  },
+  {
+    key: "contentType",
+    label: "File Type",
+  },
+  {
+    key: "size",
+    label: "Size",
+  },
+  {
+    key: "uploadedAt",
+    label: "Uploaded",
+  },
+  {
+    key: "actions",
+    label: "Actions",
+  },
+];
+
+const q = ref("");
+const page = ref(1);
+const perPage = ref(5);
+
+const formatFileSize = (bytes: number) => {
+  if (bytes === 0) return "0 Bytes";
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+};
+
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+};
+
+const filteredRows = computed(() => {
+  let rows = data.value || [];
+
+  if (q.value) {
+    rows = rows.filter((file) => {
+      return (
+        file.pathname.toLowerCase().includes(q.value.toLowerCase()) ||
+        file?.contentType?.toLowerCase().includes(q.value.toLowerCase())
+      );
+    });
+  }
+
+  const start = (page.value - 1) * perPage.value;
+  const end = start + perPage.value;
+
+  return rows.slice(start, end);
+});
+</script>
+>>>>>>> f67f057 (remove unused components, API endpoints, and update nuxt.config to streamline the application)
