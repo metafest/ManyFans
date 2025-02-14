dfa015d1c0cf52119f038b74234d20cb9ca7ed30
BalajiSriraman <balajisrdev@gmail.com>
2025-02-14 23:15:23 +0530

    add GitHub Actions workflow for NuxtHub deployment and update nuxt.config for development environment

--- apps/server/nuxt.config.ts
+++ apps/server/nuxt.config.ts
@@ -2,15 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-07-30",
   future: { compatibilityVersion: 4 },
-  modules: [
-    "@nuxthub/core",
-    "@nuxt/fonts",
-    "@nuxt/ui",
-    "@vueuse/nuxt",
-    "nuxt-auth-utils",
-  ],
+  modules: ["@nuxthub/core", "@nuxt/ui", "@vueuse/nuxt", "nuxt-auth-utils"],
   hub: {
     blob: true,
+  },
+  $development: {
+    hub: {
+      remote: true,
+    },
   },
   experimental: {
     viewTransition: true,
