366a54ff4c6b9ec9bc21520c18d4b6e9ed5ef520
BalajiSriraman <balajisrdev@gmail.com>
2025-02-17 09:14:24 +0530

    add Radix UI components for Collapsible and Dropdown Menu; implement Breadcrumb and Sidebar navigation

---
+++ apps/client/src/components/app-sidebar.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import * as React from "react";
+import {
+  AudioWaveform,
+  Command,
+  GalleryVerticalEnd,
+  Map,
+  PersonStandingIcon,
+  File,
+  Settings2,
+} from "lucide-react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import FanIcon from "@/assests/icon/fan";
+import { FanHandler } from "./common/FanHandler";
+
+// This is sample data.
+const data = {
+  user: {
+    name: "Balaji Sriraman",
+    email: "balaji@mom.io",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Playground",
+      url: "/",
+      icon: Settings2,
+
+      isActive: true,
+      items: [
+        {
+          title: "History",
+          url: "#",
+        },
+        {
+          title: "Starred",
+          url: "#",
+        },
+        {
+          title: "Settings",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  projects: [
+    {
+      name: "File Upload",
+      url: "#",
+      icon: File,
+    },
+    {
+      name: "p2p Sharing",
+      url: "#",
+      icon: PersonStandingIcon,
+    },
+    {
+      name: "Video Streaming",
+      url: "#",
+      icon: Map,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <FanHandler />
+        {/* <TeamSwitcher teams={data.teams} /> */}
+      </SidebarHeader>
+      <SidebarContent>
+        {/* Comming Soon */}
+        {/* <NavMain items={data.navMain} /> */}
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
