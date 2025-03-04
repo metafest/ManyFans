8aa409cfc78d7971e53608bb3f0424fc7ac4e2e0
BalajiSriraman <balajisrdev@gmail.com>
2025-03-04 23:27:10 +0530

    Enhance file management and upload functionality

--- apps/server/server/api/files/[pathname].get.ts
+++ apps/server/server/api/files/[pathname].get.ts
@@ -1,9 +1,15 @@
-import z from 'zod'
+import z from "zod";
 
 export default eventHandler(async (event) => {
-  const { pathname } = await getValidatedRouterParams(event, z.object({
-    pathname: z.string().min(1)
-  }).parse)
+  const { pathname } = await getValidatedRouterParams(
+    event,
+    z.object({
+      pathname: z.string().min(1),
+    }).parse
+  );
 
-  return hubBlob().serve(event, pathname)
-})
+  // Set Content-Disposition header to trigger file download
+  // setHeader(event, "Content-Disposition", `inline;filename=${pathname}`);
+
+  return hubBlob().serve(event, pathname);
+});
